767c2abb5926c1ce40dccae3c675d8ca8b8541e1
songhee <thdgml5537@gmail.com>
2021-01-19 14:48:05 -0500

    change to Hash router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import About from "./components/About/About";
 import Discover from "./components/Discover/Discover";
 import Search from "./components/Search/Search";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Route exact path="/" component={About} />
       <Route exact path="/Discover" component={Discover} />
       <Route exact path="/Search" component={Search} />
